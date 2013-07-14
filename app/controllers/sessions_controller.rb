ab25dd28c6ba3a380af37a9d9f7ec2f271708c70
dtredger <d.tredger@gmail.com>
2013-07-14 16:22:02 -0400

    attempted admin pages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,5 +16,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to root_path, :notice => "You have signed out"
   end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,9 +3,37 @@ module SessionsHelper
   def sign_in(user)
     cookies.permanent[:remember_token] = user.remember_token
     self.current_user = user
+
   end
 
   def current_user=(user)
     @current_user = user
   end
+
+  def current_user?(user)
+    user == current_user
+  end
+
+  def signed_in?
+    if @current_user != nil
+      return true
+    else
+      return false
+    end
+  end
+
+  def signed_in_user
+    unless signed_in?
+      redirect_to new_session_path, notice: "Please sign in."
+    end
+  end
+
+  def is_admin?(user)
+    user.role == "admin"
+  end
+
+  # def signed_in_as_role
+  #   if is_admin?
+
+  #   else
 end
